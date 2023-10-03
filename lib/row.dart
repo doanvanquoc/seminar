d50bbcc12bd291245f4e697049c6e02e52c2188e
LAPTOP-89CSI2HV\VIHAO <nguyenhaovkag@gmail.com>
2023-10-04 00:39:34 +0700

    add new file colum and row in folder lib

---
+++ lib/row.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyRow extends StatelessWidget {
+  const MyRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // mainAxisSize: MainAxisSize.max,
+      // mainAxisAlignment: MainAxisAlignment.center,
+      // crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink, foregroundColor: Colors.white),
+            child: const Text(
+              "Nhấn đi",
+              style: TextStyle(fontSize: 20),
+            )),
+        TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.pink, foregroundColor: Colors.white),
+            child: const Text(
+              "Nhấn đi",
+              style: TextStyle(fontSize: 20),
+            )),
+        OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.pink, foregroundColor: Colors.white),
+            child: const Text("Nhấn đi"))
+      ],
+    );
+  }
+}

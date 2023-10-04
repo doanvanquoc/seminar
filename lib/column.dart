da360f6942206c7ceaf58d936e6d489fc0d52496
Đoàn Văn Quốc <vanquoc.dev@gmail.com>
2023-10-04 10:18:19 +0700

    fix bug

--- lib/column.dart
+++ lib/column.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_coban/row.dart';
 
 class MyColumn extends StatelessWidget {
   const MyColumn({super.key});

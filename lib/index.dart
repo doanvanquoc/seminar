b663c92f88c20a9a954b1236e5bdd09e1841cc35
Đoàn Văn Quốc <vanquoc.dev@gmail.com>
2023-10-16 19:51:25 +0700

    merrge

--- lib/index.dart
+++ lib/index.dart
@@ -4,9 +4,14 @@ class IndexScreen extends StatelessWidget {
   const IndexScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      appBar: AppBar(title: const Text('Trang Chủ',
-      style: TextStyle(color: Colors.blue, fontSize: 20),),),
+      appBar: AppBar(
+        title: const Text(
+          'Trang Chủ',
+          style: TextStyle(color: Colors.blue, fontSize: 20),
+        ),
+      ),
       body: const Text('Index Screen'),
     );
   }
